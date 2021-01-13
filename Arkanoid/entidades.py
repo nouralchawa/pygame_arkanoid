1c6bb377ee18caec7a3b9a3de12751fcbfe65d11
Nour <nouralchawa@example.com>
2021-01-14 00:56:36 +0100

    Pelota que rebota

--- Arkanoid/entidades.py
+++ Arkanoid/entidades.py
@@ -6,9 +6,78 @@ import sys
 
 pg.init()
 
+class Ladrillo:
+    w=64
+    h=32
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+        self.imagen = pg.Surface ((self.w, self.h))
+        self.imagen.fill((255,255,255))
+        
+
+
+    @property
+    def rect(self):
+        return self.imagen.get_rect(topleft=(self.x, self.y))
+
+    def actualizar(self):
+        pass
+
+    def comprobar_colision(self, algo):
+        pass
+
+
+class Raqueta:
+    def __init__(self, x, y, vx):
+        self.x = x
+        self.y = y
+        self.vx =vx
+
+        self.imagen = pg.image.load("resources/images/regular_racket.png")
+        
+
+    def actualizar(self):
+        self.x += self.vx
+        if self.x + 128 >= GAME_DIMENSIONS[0]:
+            self.x = GAME_DIMENSIONS[0] - 128
+        if self.x <= 0:
+            self.x = 0
+
+    @property
+    def rect(self):
+        return self.imagen.get_rect(topleft=(self.x, self.y))
+
+    def manejar_eventos(self):
+        '''
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_RIGHT:
+                    self.x += 10
+                    if self.x +128 >= GAME_DIMENSIONS[0]:
+                        self.x = GAME_DIMENSIONS[0] - 128
+                    
+                    
+                if event.key == pg.K_LEFT:
+                    self.x -= 10
+                    if self.x <= 0:
+                        self.x = 
+                        '''
+                    
+        teclas_pulsadas = pg.key.get_pressed()
+        if teclas_pulsadas[pg.K_RIGHT]:
+            self.vx = 10
+        elif teclas_pulsadas [pg.K_LEFT]:
+            self.vx = -10
+        else:
+            self.vx = 0
+
+
+
 class Pelota():
     imagenes_files = ['brown_ball.png', 'blue_ball.png', 'red_ball.png', 'green_ball.png']
     num_imgs_explosion = 8
+    retardo_animaciones = 5
 
     def __init__(self, x, y, vx, vy):
         self.x = x
@@ -19,6 +88,10 @@ class Pelota():
         self.imagenes_explosion = self.cargaExplosion()
         self.imagen_act = 0
         self.ix_explosion = 0
+        self.retardo_animaciones = 5
+        self.ciclos_tras_refresco = 0
+        self.ticks_acumulados = 0
+        self.ticks_por_frame_de_animacion = 1000//FPS*self.retardo_animaciones
         self.muriendo = False
         self.imagen = self.imagenes[self.imagen_act]
 
@@ -58,6 +131,7 @@ class Pelota():
 
         if self.rect.bottom >= GAME_DIMENSIONS[1]:
             self.muriendo = True
+            self.ciclos_tras_refresco = 0
             return
             
             
@@ -68,48 +142,98 @@ class Pelota():
         '''
         Gestionar imagen activa (disfraz) de pelota
         '''
-        self.imagen_act += 1
-        if self.imagen_act >= len(self.imagenes):
-            self.imagen_act = 0
-        self.imagen = self.imagenes[self.imagen_act]   
+        self.ciclos_tras_refresco += 1
+        
+        if self.ciclos_tras_refresco % self.retardo_animaciones == 0:
+            self.imagen_act += 1
+            if self.imagen_act >= len(self.imagenes):
+                self.imagen_act = 0
+        
+        self.imagen = self.imagenes[self.imagen_act]
+        
 
-    def actualizar(self):
+    def actualizar(self, dt):
         self.actualizar_posicion()
 
         if self.muriendo:
-            self.explosion()
+            return self.explosion(dt)
         else:
             self.actualizar_disfraz
+
         
-    def explosion(self):
+    def explosion(self, dt):
         if self.ix_explosion >= len(self.imagenes_explosion):
-            self.ix_explosion = 0
+            return True
+            
         self.imagen = self.imagenes_explosion[self.ix_explosion]
-        self.ix_explosion += 1
+
+        self.ciclos_tras_refresco += 1
+
+        self.ticks_acumulados += dt
+        if self.ticks_acumulados >= self.ticks_por_frame_de_animacion:
+            self.ix_explosion += 1
+            self.ticks_acumulados = 0
+        return False
+
+    def comprobar_colision(self, algo):
+        if (self.rect.left >= algo.rect.left and self.rect.left <= algo.rect.right or \
+        self.rect.right >= algo.rect.left and self.rect.right <= algo.rect.right) and \
+        self.rect.bottom >= algo.rect.top:
+
+           self.vy *= -1
 
 
 class Game:
     def __init__(self):
         self.pantalla = pg.display.set_mode(GAME_DIMENSIONS)
         pg.display.set_caption("Futuro Arkanoid")
+
         self.pelota = Pelota(400, 300, 5, 5)
+        self.raqueta = Raqueta(336, 550, 0)
+
+        self.El_ladrillo = (400, 300)
+        self.El_ladrillo = None
+        self.ladrillos = []
+        xo = 16
+        yo = 16
+        for c in range(12):
+            for f in range(5):
+                l = Ladrillo(xo + c * Ladrillo.w, yo + f * Ladrillo.h)
+                self.ladrillos.append(l)
+        
         self.clock = pg.time.Clock()
 
     def bucle_principal(self):
         game_over = False
         while not game_over:
-            milisegundos = self.clock.tick(FPS)
+            dt = self.clock.tick(FPS)
+            '''
+            Gestion de eventos
+            '''
             events = pg.event.get()
             for event in events:
                 if event.type == pg.QUIT:
                     pg.quit()
                     sys.exit()
 
-            game_over = self.pelota.actualizar()
+            self.raqueta.manejar_eventos()
+            
+            '''
+            Actualizacion de elementos del juego
+            '''
+            game_over = self.pelota.actualizar(dt)
+            self.raqueta.actualizar()
+            self.pelota.comprobar_colision(self.raqueta)
 
             self.pantalla.fill((150,10,150))
             self.pantalla.blit(self.pelota.imagen, (self.pelota.x, self.pelota.y))
+            self.pantalla.blit(self.raqueta.imagen, (self.raqueta.x, self.raqueta.y))
             
-
+            for ladrillo in self.ladrillos:
+                self.pantalla.blit(ladrillo.imagen, (ladrillo.x, ladrillo.y))
+            
+            '''
+            Refrescar pantalla
+            '''
             pg.display.flip()
 
